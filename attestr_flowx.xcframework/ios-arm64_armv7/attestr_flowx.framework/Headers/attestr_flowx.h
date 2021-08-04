ab91371cabdf481d2b0909203788ae6933fa1dcc
Tushar Kalra <tushar.gupta0303@gmail.com>
2021-08-04 13:23:25 +0530

    Change header file name

---
+++ attestr_flowx.xcframework/ios-arm64_armv7/attestr_flowx.framework/Headers/attestr_flowx.h
@@ -0,0 +1,19 @@
+//
+//  attestr_flowx.h
+//  attestr_flowx
+//
+//  Created by Tushar Kalra
+//  Copyright (c) 2021 Pegadroid IQ Solutions Pvt. Ltd
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for attestr_flowx.
+FOUNDATION_EXPORT double attestr_flowxVersionNumber;
+
+//! Project version string for attestr_flowx.
+FOUNDATION_EXPORT const unsigned char attestr_flowxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <attestr_flowx/PublicHeader.h>
+
+
